f4ee4bcd170ec11ff7e03947c5bf3ac8d92160d0
Florent-V <vasseurflorent@gmail.com>
2025-09-15 08:48:37 +0200

    feat(end-game): handle finish/abort game

--- src/Controller/Quiz/AbortController.php
+++ src/Controller/Quiz/AbortController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller\Quiz;
 
 use App\Entity\QuizSession;
-use App\Entity\User;
 use App\Enum\QuizSessionStatus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +14,7 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route(
     '/quiz/{id}/abort',
     name: 'app_quiz_abort',
-    methods: ['POST']
+    methods: ['GET']
 )]
 class AbortController extends AbstractController
 {
@@ -24,25 +23,21 @@ class AbortController extends AbstractController
         EntityManagerInterface $entityManager,
     ): Response {
         // Security check: only the user who started the quiz can abort it.
-        // Guests (user is null) can abort their own quizzes.
-        if ($quizSession->getUser() !== $this->getUser()) {
-            return $this->json(
-                [
-                    'error' => 'Vous n\'êtes pas autorisé à abandonner ce quiz.',
-                ],
-                Response::HTTP_FORBIDDEN
-            );
+        if ($this->getUser() !== $quizSession->getUser()) {
+            $this->addFlash('error', 'Vous n\'êtes pas autorisé à terminer ce quiz.');
+            throw $this->createAccessDeniedException();
         }
 
         // Prevent aborting a quiz that is already finished or aborted.
         if (in_array($quizSession->getStatus(), [QuizSessionStatus::Finished, QuizSessionStatus::Cancelled])) {
-            return $this->json(['message' => 'Ce quiz est déjà terminé ou abandonné.'], Response::HTTP_OK);
+            $this->addFlash('error', 'Vous n\'êtes pas autorisé à terminer ce quiz.');
+            throw $this->createAccessDeniedException();
         }
 
         $quizSession->setStatus(QuizSessionStatus::Cancelled);
         $quizSession->setFinishedAt(new \DateTime());
         $entityManager->flush();
 
-        return $this->json(['message' => 'Quiz abandonné avec succès.'], Response::HTTP_OK);
+        return $this->redirectToRoute('app_home');
     }
 }
