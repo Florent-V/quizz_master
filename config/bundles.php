8e4193a774c23ba1caaebf511de42edc1560a65a
Florent-V <vasseurflorent@gmail.com>
2025-09-28 14:16:32 +0200

    feat(player-stat): add knp paginator

--- config/bundles.php
+++ config/bundles.php
@@ -31,4 +31,5 @@ return [
     Symfony\UX\Chartjs\ChartjsBundle::class                                  => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class                                => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class                      => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class                     => ['all' => true],
 ];
